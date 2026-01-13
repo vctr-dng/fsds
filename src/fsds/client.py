a0ae79976af68d3749de59830f24e4a21e5bfd8f
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2026-01-13 00:31:47 -0500

    style: reformat with ruff

--- src/fsds/client.py
+++ src/fsds/client.py
@@ -3,18 +3,19 @@ from __future__ import print_function
 from .utils import *
 from .types import *
 
-import msgpackrpc # pip install msgpack-rpc-python
-import numpy as np # pip install numpy
-import msgpack
-import time
-import math
-import logging
+import msgpackrpc
+
 
 class FSDSClient:
-    def __init__(self, ip = "", port = 41451, timeout_value = 3):
-        if (ip == ""):
+    def __init__(self, ip="", port=41451, timeout_value=3):
+        if ip == "":
             ip = "127.0.0.1"
-        self.client = msgpackrpc.Client(msgpackrpc.Address(ip, port), timeout = timeout_value, pack_encoding = 'utf-8', unpack_encoding = 'utf-8')
+        self.client = msgpackrpc.Client(
+            msgpackrpc.Address(ip, port),
+            timeout=timeout_value,
+            pack_encoding="utf-8",
+            unpack_encoding="utf-8",
+        )
 
     # -----------------------------------  Common vehicle APIs ---------------------------------------------
     def reset(self):
@@ -23,7 +24,7 @@ class FSDSClient:
 
         Note that you must call `enableApiControl` again after the call to reset
         """
-        self.client.call('reset')
+        self.client.call("reset")
 
     def ping(self):
         """
@@ -32,9 +33,9 @@ class FSDSClient:
         Returns:
             bool:
         """
-        return self.client.call('ping')
+        return self.client.call("ping")
 
-    def enableApiControl(self, is_enabled, vehicle_name = 'FSCar'):
+    def enableApiControl(self, is_enabled, vehicle_name="FSCar"):
         """
         Enables or disables API control for vehicle corresponding to vehicle_name
 
@@ -42,9 +43,9 @@ class FSDSClient:
             is_enabled (bool): True to enable, False to disable API control
             vehicle_name (str, optional): Name of the vehicle to send this command to
         """
-        self.client.call('enableApiControl', is_enabled, vehicle_name)
+        self.client.call("enableApiControl", is_enabled, vehicle_name)
 
-    def isApiControlEnabled(self, vehicle_name = 'FSCar'):
+    def isApiControlEnabled(self, vehicle_name="FSCar"):
         """
         Returns true if API control is established.
 
@@ -56,7 +57,7 @@ class FSDSClient:
         Returns:
             bool: If API control is enabled
         """
-        return self.client.call('isApiControlEnabled', vehicle_name)
+        return self.client.call("isApiControlEnabled", vehicle_name)
 
     def confirmConnection(self):
         """
@@ -66,13 +67,13 @@ class FSDSClient:
         if self.ping():
             print("Ping to simulator OK")
         else:
-             print("Ping to simulator FAIL")
-             exit(1)
+            print("Ping to simulator FAIL")
+            exit(1)
 
     # camera control
     # simGetImage returns compressed png in array of bytes
     # image_type uses one of the ImageType members
-    def simGetImage(self, camera_name, image_type, vehicle_name = 'FSCar'):
+    def simGetImage(self, camera_name, image_type, vehicle_name="FSCar"):
         """
         Get a single image
 
@@ -92,15 +93,15 @@ class FSDSClient:
         camera_name = str(camera_name)
 
         # because this method returns std::vector<uint8>, msgpack decides to encode it as a string unfortunately.
-        result = self.client.call('simGetImage', camera_name, image_type, vehicle_name)
-        if (result == "" or result == "\0"):
+        result = self.client.call("simGetImage", camera_name, image_type, vehicle_name)
+        if result == "" or result == "\0":
             return None
         return result
 
     # camera control
     # simGetImage returns compressed png in array of bytes
     # image_type uses one of the ImageType members
-    def simGetImages(self, requests, vehicle_name = 'FSCar'):
+    def simGetImages(self, requests, vehicle_name="FSCar"):
         """
         Get multiple images
 
@@ -113,11 +114,12 @@ class FSDSClient:
         Returns:
             list[ImageResponse]:
         """
-        responses_raw = self.client.call('simGetImages', requests, vehicle_name)
-        return [ImageResponse.from_msgpack(response_raw) for response_raw in responses_raw]
+        responses_raw = self.client.call("simGetImages", requests, vehicle_name)
+        return [
+            ImageResponse.from_msgpack(response_raw) for response_raw in responses_raw
+        ]
 
-
-    def simGetGroundTruthKinematics(self, vehicle_name = 'FSCar'):
+    def simGetGroundTruthKinematics(self, vehicle_name="FSCar"):
         """
         Get Ground truth kinematics of the vehicle
 
@@ -127,12 +129,13 @@ class FSDSClient:
         Returns:
             KinematicsState: Ground truth of the vehicle
         """
-        kinematics_state = self.client.call('simGetGroundTruthKinematics', vehicle_name)
+        kinematics_state = self.client.call("simGetGroundTruthKinematics", vehicle_name)
         return KinematicsState.from_msgpack(kinematics_state)
-    simGetGroundTruthKinematics.__annotations__ = {'return': KinematicsState}
+
+    simGetGroundTruthKinematics.__annotations__ = {"return": KinematicsState}
 
     # sensor APIs
-    def getLidarData(self, lidar_name = '', vehicle_name = 'FSCar'):
+    def getLidarData(self, lidar_name="", vehicle_name="FSCar"):
         """
         Args:
             lidar_name (str, optional): Name of Lidar to get data from, specified in settings.json
@@ -140,9 +143,11 @@ class FSDSClient:
         Returns:
             LidarData:
         """
-        return LidarData.from_msgpack(self.client.call('getLidarData', lidar_name, vehicle_name))
+        return LidarData.from_msgpack(
+            self.client.call("getLidarData", lidar_name, vehicle_name)
+        )
 
-    def getImuData(self, imu_name = '', vehicle_name = 'FSCar'):
+    def getImuData(self, imu_name="", vehicle_name="FSCar"):
         """
         Args:
             imu_name (str, optional): Name of IMU to get data from, specified in settings.json. When no name is provided the last imu will be used.
@@ -151,9 +156,11 @@ class FSDSClient:
         Returns:
             ImuData:
         """
-        return ImuData.from_msgpack(self.client.call('getImuData', imu_name, vehicle_name))
+        return ImuData.from_msgpack(
+            self.client.call("getImuData", imu_name, vehicle_name)
+        )
 
-    def getGpsData(self, gps_name = '', vehicle_name = 'FSCar'):
+    def getGpsData(self, gps_name="", vehicle_name="FSCar"):
         """
         Args:
             gps_name (str, optional): Name of GPS to get data from, specified in settings.json
@@ -162,18 +169,22 @@ class FSDSClient:
         Returns:
             GpsData:
         """
-        return GpsData.from_msgpack(self.client.call('getGpsData', gps_name, vehicle_name))
+        return GpsData.from_msgpack(
+            self.client.call("getGpsData", gps_name, vehicle_name)
+        )
 
-    def getGroundSpeedSensorData(self, vehicle_name = 'FSCar'):
+    def getGroundSpeedSensorData(self, vehicle_name="FSCar"):
         """
         Args:
             vehicle_name (str, optional): Name of vehicle to which the sensor corresponds to. Default FSCar.
         Returns:
             GroundSpeedSensorData:
         """
-        return GroundSpeedSensorData.from_msgpack(self.client.call('getGroundSpeedSensorData', vehicle_name))
+        return GroundSpeedSensorData.from_msgpack(
+            self.client.call("getGroundSpeedSensorData", vehicle_name)
+        )
 
-    def setCarControls(self, controls, vehicle_name = 'FSCar'):
+    def setCarControls(self, controls, vehicle_name="FSCar"):
         """
         Control the car using throttle, steering, brake, etc.
 
@@ -181,9 +192,9 @@ class FSDSClient:
             controls (CarControls): Struct containing control values
             vehicle_name (str, optional): Name of vehicle to be controlled
         """
-        self.client.call('setCarControls', controls, vehicle_name)
+        self.client.call("setCarControls", controls, vehicle_name)
 
-    def getCarState(self, vehicle_name = 'FSCar'):
+    def getCarState(self, vehicle_name="FSCar"):
         """
         Args:
             vehicle_name (str, optional): Name of vehicle
@@ -191,11 +202,11 @@ class FSDSClient:
         Returns:
             CarState:
         """
-        state_raw = self.client.call('getCarState', vehicle_name)
+        state_raw = self.client.call("getCarState", vehicle_name)
         return CarState.from_msgpack(state_raw)
 
     def getRefereeState(self):
-        referee_state_raw = self.client.call('getRefereeState')
+        referee_state_raw = self.client.call("getRefereeState")
         return RefereeState.from_msgpack(referee_state_raw)
 
     def getSettingsString(self):
@@ -204,4 +215,4 @@ class FSDSClient:
         Returns:
             str: Settings text in JSON format
         """
-        return self.client.call('getSettingsString')
+        return self.client.call("getSettingsString")
